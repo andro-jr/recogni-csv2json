93898db75de9dc658e848e28ff250d853d19c1fb
andro-jr <prabin20panta@gmail.com>
2024-06-12 15:54:06 +0545

    parameters csv to json

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const csv = require("csvtojson");
 const fs = require("fs");
 const path = require("path");
 
+const INPUT_ROOT_DIR = "csv";
+const OUTPUT_ROOT_DIR = "json";
+
 const dmFilePath = path.join(__dirname, "csv/diffusionModel.csv");
 const llmCsvPath = path.join(__dirname, "csv/largelanguagemodel.csv");
 const prmoptsCsvPath = path.join(__dirname, "csv/prompts.csv");
+const modelsCsvPath = path.join(__dirname, "csv/models.csv");
+const paramsCsvPath = path.join(__dirname, "csv/parameters.csv");
 
 // Convert DM CSV to JSON
 csv()
@@ -41,7 +46,7 @@ csv()
     };
 
     fs.writeFileSync(
-      "json/diffusionModel.json",
+      `${OUTPUT_ROOT_DIR}/diffusionModel.json`,
       JSON.stringify(wrappedData, null, 2)
     );
     console.log(
@@ -89,7 +94,7 @@ csv()
     };
 
     fs.writeFileSync(
-      "json/largeLanguageModel.json",
+      `${OUTPUT_ROOT_DIR}/largeLanguageModel.json`,
       JSON.stringify(wrappedData, null, 2)
     );
     console.log(
@@ -100,7 +105,7 @@ csv()
     console.error("2) Error converting LargeLanguageModel CSV to JSON:", error);
   });
 
-// Convert Prompts CSV to JSON
+// Convert Models CSV to JSON
 csv()
   .fromFile(prmoptsCsvPath)
   .then((jsonObj) => {
@@ -120,9 +125,80 @@ csv()
       prompts: structuredData,
     };
 
-    fs.writeFileSync("json/prompts.json", JSON.stringify(wrappedData, null, 2));
+    fs.writeFileSync(
+      `${OUTPUT_ROOT_DIR}/prompts.json`,
+      JSON.stringify(wrappedData, null, 2)
+    );
     console.log("2) Prompts CSV data has been converted to JSON successfully.");
   })
   .catch((error) => {
     console.error("2) Error converting Prompts CSV to JSON:", error);
+  });
+
+// Convert Parameters CSV to JSON
+csv()
+  .fromFile(modelsCsvPath)
+  .then((jsonObj) => {
+    // Define the JSON structure based on the actual column names in your CSV file
+    const structuredData = jsonObj
+      .filter((row) => Number(Object.values(row)[0]) !== 0)
+      .map((row) => {
+        return {
+          name: Object.values(row)[0],
+          type: Object.values(row)[1],
+        };
+      });
+
+    const wrappedData = {
+      models: structuredData,
+    };
+
+    fs.writeFileSync(
+      `${OUTPUT_ROOT_DIR}/models.json`,
+      JSON.stringify(wrappedData, null, 2)
+    );
+    console.log("2) Models CSV data has been converted to JSON successfully.");
+  })
+  .catch((error) => {
+    console.error("2) Error converting Models CSV to JSON:", error);
+  });
+// Convert Prompts CSV to JSON
+csv()
+  .fromFile(paramsCsvPath)
+  .then((jsonObj) => {
+    let batchArray = [];
+    let tensorArray = [];
+    // Define the JSON structure based on the actual column names in your CSV file
+    jsonObj
+      .filter(
+        (row) =>
+          Number(Object.values(row)[0]) !== 0 ||
+          Number(Object.values(row)[1]) !== 0
+      )
+      .forEach((row) => {
+        if (Number(Object.values(row)[0]) !== 0)
+          batchArray.push(Number(Object.values(row)[0]));
+        if (Number(Object.values(row)[1]) !== 0)
+          tensorArray.push(Number(Object.values(row)[1]));
+      });
+
+    const structuredData = {
+      batchSize: batchArray,
+      tensorParallelism: tensorArray,
+    };
+
+    const wrappedData = {
+      parameters: structuredData,
+    };
+
+    fs.writeFileSync(
+      `${OUTPUT_ROOT_DIR}/parameters.json`,
+      JSON.stringify(wrappedData, null, 2)
+    );
+    console.log(
+      "2) Parameters CSV data has been converted to JSON successfully."
+    );
+  })
+  .catch((error) => {
+    console.error("2) Error converting Parameters CSV to JSON:", error);
   });
