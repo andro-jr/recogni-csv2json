2691921214c6bcd0cb56b247a33b50df0f369c92
andro-jr <prabin20panta@gmail.com>
2024-06-12 15:03:34 +0545

    csv parsing for llm

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const csv = require("csvtojson");
 const fs = require("fs");
-
-// Path to the CSV file
-
 const path = require("path");
 
-// Path to your Excel file
-const csvFilePath = path.join(__dirname, "diffusionModel");
-console.log("csvFilePath :", csvFilePath);
+const dmFilePath = path.join(__dirname, "csv/diffusionModel.csv");
+const llmCsvPath = path.join(__dirname, "csv/largelanguagemodel.csv");
 
 // Convert CSV to JSON
 csv()
-  .fromFile(csvFilePath)
+  .fromFile(dmFilePath)
   .then((jsonObj) => {
     // Define the JSON structure based on the actual column names in your CSV file
     const structuredData = jsonObj.slice(1).map((row) => {
@@ -44,9 +40,62 @@ csv()
     };
 
     // Write the structured data to a JSON file
-    fs.writeFileSync("output.json", JSON.stringify(wrappedData, null, 2));
-    console.log("CSV data has been converted to JSON successfully.");
+    fs.writeFileSync(
+      "json/diffusionModel.json",
+      JSON.stringify(wrappedData, null, 2)
+    );
+    console.log(
+      "1) DiffusionModel CSV data has been converted to JSON successfully."
+    );
   })
   .catch((error) => {
-    console.error("Error converting CSV to JSON:", error);
+    console.error("1) Error converting DiffusionModel CSV CSV to JSON:", error);
+  });
+
+csv()
+  .fromFile(llmCsvPath)
+  .then((jsonObj) => {
+    // Define the JSON structure based on the actual column names in your CSV file
+    const structuredData = jsonObj.slice(1).map((row) => {
+      return {
+        prompt: Number(Object.values(row)[1]),
+        batchSize: Number(Object.values(row)[2]),
+        tensorParallelism: Number(Object.values(row)[3]),
+        model: Object.values(row)[4],
+        recogni: {
+          TTFT: Number(Object.values(row)[5]),
+          TPOT: Number(Object.values(row)[6]),
+          "Tokens/Sec": Number(Object.values(row)[7]),
+          "Tokens/Sec/User": Number(Object.values(row)[8]),
+          "Total Power Consumption": Number(Object.values(row)[9]),
+          TCO: Number(Object.values(row)[10]),
+          Efficiency: Number(Object.values(row)[11]),
+        },
+        competitor: {
+          TTFT: Number(Object.values(row)[12]),
+          TPOT: Number(Object.values(row)[13]),
+          "Tokens/Sec": Number(Object.values(row)[14]),
+          "Tokens/Sec/User": Number(Object.values(row)[15]),
+          "Total Power Consumption": Number(Object.values(row)[16]),
+          TCO: Number(Object.values(row)[17]),
+          Efficiency: Number(Object.values(row)[18]),
+        },
+      };
+    });
+
+    const wrappedData = {
+      largeLanguageModel: structuredData,
+    };
+
+    // Write the structured data to a JSON file
+    fs.writeFileSync(
+      "json/largeLanguageModel.json",
+      JSON.stringify(wrappedData, null, 2)
+    );
+    console.log(
+      "2) LargeLanguageModel CSV data has been converted to JSON successfully."
+    );
+  })
+  .catch((error) => {
+    console.error("2) Error converting LargeLanguageModel CSV to JSON:", error);
   });
